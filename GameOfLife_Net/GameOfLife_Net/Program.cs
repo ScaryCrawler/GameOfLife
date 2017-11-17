9fc3911791dadaef403d808d91b80079a37b3dc7
ISakhno <ivan.sakhno@firstlinesoftware.com>
2017-11-17 15:56:54 +0300

    Starting implementation (test file, reading from this this file)

--- GameOfLife_Net/GameOfLife_Net/Program.cs
+++ GameOfLife_Net/GameOfLife_Net/Program.cs
@@ -1,9 +1,44 @@
-﻿namespace GameOfLife_Net
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace GameOfLife_Net
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            if(args.Length != 1)
+                throw new ArgumentException("Invalid argument usage");
+
+            int[,] currentStepArray;
+            int[,] nextStepArray;
+
+            int rowCount;
+            int colCount;
+
+            using (StreamReader reader = new StreamReader(args[0]))
+            {
+                var buffer = reader.ReadToEnd();
+                var rows = buffer.Split('\n').Select(x => x.Split(' ')).ToList();
+
+                rowCount = rows.Count;
+                colCount = rows[0].Length;
+
+                currentStepArray = new int[rowCount, colCount];
+                nextStepArray = new int[rowCount, colCount];
+
+                for (int i = 0; i < rowCount; i++)
+                {
+                    for (int j = 0; j < colCount; j++)
+                    {
+                        currentStepArray[i, j] = Int32.Parse(rows[i][j]);
+                        nextStepArray[i, j] = currentStepArray[i, j];
+                    }
+                }
+            }
+            
+
         }
     }
 }
---
+++ GameOfLife_Net/Tests/InputFileTests.cs
@@ -0,0 +1,16 @@
+﻿using GameOfLife_Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class InputFileTests
+    {
+        [TestMethod]
+        public void OnFileExisting()
+        {
+            string path = "StartedState.txt";
+            Assert.AreEqual(true, FileChecker.CheckFile(path));
+        }
+    }
+}
